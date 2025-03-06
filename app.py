04fd90f4f2701d08435853f0804310b239f6dafb
raviteja0828 <merakanapalliraviteja809@gmail.com>
2025-03-06 20:50:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,11 @@ import base64
 import os
 
 
+# S3 configuration
 s3_client = boto3.client('s3',
-                         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-                         region_name=os.getenv('AWS_DEFAULT_REGION'))  # Replace with your region, e.g., 'us-east-1'
+                         aws_access_key_id='AKIA4IM3HSVQ6TCYASDR',  # Replace with your access key
+                         aws_secret_access_key='WlcfnJ3W9bcuL1/PRaFXaHjV07afRkJUKIHl4u1Z',  # Replace with your secret key
+                         region_name='eu-north-1')  # Replace with your region, e.g., 'us-east-1'
 
 BUCKET_NAME = 'ashu2807'  # Replace with your S3 bucket name
 
