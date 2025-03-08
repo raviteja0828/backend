46b523e703079bbb0e6cd64b959109a101e64f80
raviteja0828 <merakanapalliraviteja809@gmail.com>
2025-03-08 21:05:28 +0530

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const User = require("./models/user.js");
+const User = require("./models/user");
 
 const router = express.Router();
 
