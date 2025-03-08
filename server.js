8d82d61e194b13350ed882f47de2b5fdb138a007
raviteja0828 <merakanapalliraviteja809@gmail.com>
2025-03-09 01:17:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
+const cors = require('cors');
+
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow React app origin
-  credentials: true // Allow cookies to be sent
+    origin: ['http://localhost:3000', 'https://localhost'], // Allow both web and Capacitor apps
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
